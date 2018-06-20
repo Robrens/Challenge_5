bfa1dde84e539a3ad0a38cad1e5b9974981e352d
Robrens <jbb.baudu@wanadoo.fr>
2018-06-20 10:45:20 +0200

    I still want to die but now i will immolate my self ....

--- main.js
+++ main.js
@@ -2,8 +2,12 @@ $(document).ready(function() {
     $('#azerty').css('display','none');
     $('#search').click(function() {
         $('#azerty').css('display','block')
+        $('#azerty').focus()
     });
     $('#azerty').focusout(function() {
         $(this).css('display','none')
     });
+
+
+
 });
